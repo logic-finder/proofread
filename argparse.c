b30699a93676b464fc7dcf9f76d76fe8fdf496df
Doohyeon Won <logicseeker@naver.com>
2025-11-07 15:25:43 +0900

    fix: correct an error message

--- argparse.c
+++ argparse.c
@@ -244,7 +244,7 @@ static void parse_wrtmod(const char *arg, optflg_t *of) {
 
 static void parse_endopt(optflg_t *of) {
    if (of->hlp) fatal("proofread: -h with --.");
-   if (of->vsn) fatal("proofread: -h with --.");
+   if (of->vsn) fatal("proofread: -v with --.");
    of->eoo = true;
 }
 
